f4a225e637b9e993067ba892d446dbaff362b2ed
Oscar Alemán <fretshot@gmail.com>
2019-04-28 02:33:31 -0500

    added newton hacia delante/atras method

--- Assets/Scripts/gameplay.cs
+++ Assets/Scripts/gameplay.cs
@@ -28,6 +28,8 @@ public class gameplay : MonoBehaviour {
 
     private bool playerIsDead = false;
 
+    bool estaEnElProblema = false;
+
     public GameObject failCamera;
     public Camera mainCamera;
     public GameObject lostLiveCamera;
@@ -70,158 +72,165 @@ public class gameplay : MonoBehaviour {
 
     private void Update() {
 
+
         stopWatch();
 
-        if (Input.GetKeyDown(KeyCode.Keypad0) || Input.GetKeyDown(KeyCode.Alpha0)) {
-            if (numeros.Contains(0)) {
-                Destroy(GameObject.FindGameObjectWithTag("num0"));
-                numeros.Remove(0);
-                score += 1;
-                txt_score.text = score.ToString();
-                getPointsSound.Play();
-            } else {
-                //playerLost();
-                activarProblema();
-            }
-        }
+        if (estaEnElProblema == false) {
 
-        if (Input.GetKeyDown(KeyCode.Keypad1) || Input.GetKeyDown(KeyCode.Alpha1)) {
-            if (numeros.Contains(1)) {
-                Destroy(GameObject.FindGameObjectWithTag("num1"));
-                numeros.Remove(1);
-                score += 1;
-                txt_score.text = score.ToString();
-                getPointsSound.Play();
-            } else {
-                //playerLost();
-                activarProblema();
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.Keypad2) || Input.GetKeyDown(KeyCode.Alpha2)) {
-            if (numeros.Contains(2)) {
-                Destroy(GameObject.FindGameObjectWithTag("num2"));
-                numeros.Remove(2);
-                score += 1;
-                txt_score.text = score.ToString();
-                getPointsSound.Play();
-            } else {
-                //playerLost();
-                activarProblema();
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.Keypad3) || Input.GetKeyDown(KeyCode.Alpha3)) {
-            if (numeros.Contains(3)) {
-                Destroy(GameObject.FindGameObjectWithTag("num3"));
-                numeros.Remove(3);
-                score += 1;
-                txt_score.text = score.ToString();
-                getPointsSound.Play();
-            } else {
-                //playerLost();
-                activarProblema();
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.Keypad4) || Input.GetKeyDown(KeyCode.Alpha4)) {
-            if (numeros.Contains(4)) {
-                Destroy(GameObject.FindGameObjectWithTag("num4"));
-                numeros.Remove(4);
-                score += 1;
-                txt_score.text = score.ToString();
-                getPointsSound.Play();
-            } else {
-                //playerLost();
-                activarProblema();
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.Keypad5) || Input.GetKeyDown(KeyCode.Alpha5)) {
-            if (numeros.Contains(5)) {
-                Destroy(GameObject.FindGameObjectWithTag("num5"));
-                numeros.Remove(5);
-                score += 1;
-                txt_score.text = score.ToString();
-                getPointsSound.Play();
-            } else {
-                //playerLost();
-                activarProblema();
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.Keypad6) || Input.GetKeyDown(KeyCode.Alpha6)) {
-            if (numeros.Contains(6)) {
-                Destroy(GameObject.FindGameObjectWithTag("num6"));
-                numeros.Remove(6);
-                score += 1;
-                txt_score.text = score.ToString();
-                getPointsSound.Play();
-            } else {
-                //playerLost();
-                activarProblema();
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.Keypad7) || Input.GetKeyDown(KeyCode.Alpha7)) {
-            if (numeros.Contains(7)) {
-                Destroy(GameObject.FindGameObjectWithTag("num7"));
-                numeros.Remove(7);
-                score += 1;
-                txt_score.text = score.ToString();
-                getPointsSound.Play();
-            } else {
-                //playerLost();
-                activarProblema();
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.Keypad8) || Input.GetKeyDown(KeyCode.Alpha8)) {
-            if (numeros.Contains(8)) {
-                Destroy(GameObject.FindGameObjectWithTag("num8"));
-                numeros.Remove(8);
-                score += 1;
-                txt_score.text = score.ToString();
-                getPointsSound.Play();
-            } else {
-                //playerLost();
-                activarProblema();
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.Keypad9) || Input.GetKeyDown(KeyCode.Alpha9)) {
-            if (numeros.Contains(9)) {
-                Destroy(GameObject.FindGameObjectWithTag("num9"));
-                numeros.Remove(9);
-                score += 1;
-                txt_score.text = score.ToString();
-                getPointsSound.Play();
-            } else {
-                //playerLost();
-                activarProblema();
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return)){
-            if (playerIsDead == true) {
-                SceneManager.LoadScene("01");
-            }
-        }
-        
-
-        if (Input.GetKeyDown(KeyCode.Escape)) {
-            if (playerIsDead == true) {
-                SceneManager.LoadScene("00");
-            } else {
-                if (Time.timeScale == 1) {
-                    NotificationCenter.DefaultCenter().PostNotification(this, "pauseGameplay");
+            if (Input.GetKeyDown(KeyCode.Keypad0) || Input.GetKeyDown(KeyCode.Alpha0)) {
+                if (numeros.Contains(0)) {
+                    Destroy(GameObject.FindGameObjectWithTag("num0"));
+                    numeros.Remove(0);
+                    score += 1;
+                    txt_score.text = score.ToString();
+                    getPointsSound.Play();
                 } else {
-                    NotificationCenter.DefaultCenter().PostNotification(this, "resumeGameplay");
+                    //playerLost();
+                    activarProblema();
                 }
-                
+            }
+
+            if (Input.GetKeyDown(KeyCode.Keypad1) || Input.GetKeyDown(KeyCode.Alpha1)) {
+                if (numeros.Contains(1)) {
+                    Destroy(GameObject.FindGameObjectWithTag("num1"));
+                    numeros.Remove(1);
+                    score += 1;
+                    txt_score.text = score.ToString();
+                    getPointsSound.Play();
+                } else {
+                    //playerLost();
+                    activarProblema();
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.Keypad2) || Input.GetKeyDown(KeyCode.Alpha2)) {
+                if (numeros.Contains(2)) {
+                    Destroy(GameObject.FindGameObjectWithTag("num2"));
+                    numeros.Remove(2);
+                    score += 1;
+                    txt_score.text = score.ToString();
+                    getPointsSound.Play();
+                } else {
+                    //playerLost();
+                    activarProblema();
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.Keypad3) || Input.GetKeyDown(KeyCode.Alpha3)) {
+                if (numeros.Contains(3)) {
+                    Destroy(GameObject.FindGameObjectWithTag("num3"));
+                    numeros.Remove(3);
+                    score += 1;
+                    txt_score.text = score.ToString();
+                    getPointsSound.Play();
+                } else {
+                    //playerLost();
+                    activarProblema();
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.Keypad4) || Input.GetKeyDown(KeyCode.Alpha4)) {
+                if (numeros.Contains(4)) {
+                    Destroy(GameObject.FindGameObjectWithTag("num4"));
+                    numeros.Remove(4);
+                    score += 1;
+                    txt_score.text = score.ToString();
+                    getPointsSound.Play();
+                } else {
+                    //playerLost();
+                    activarProblema();
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.Keypad5) || Input.GetKeyDown(KeyCode.Alpha5)) {
+                if (numeros.Contains(5)) {
+                    Destroy(GameObject.FindGameObjectWithTag("num5"));
+                    numeros.Remove(5);
+                    score += 1;
+                    txt_score.text = score.ToString();
+                    getPointsSound.Play();
+                } else {
+                    //playerLost();
+                    activarProblema();
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.Keypad6) || Input.GetKeyDown(KeyCode.Alpha6)) {
+                if (numeros.Contains(6)) {
+                    Destroy(GameObject.FindGameObjectWithTag("num6"));
+                    numeros.Remove(6);
+                    score += 1;
+                    txt_score.text = score.ToString();
+                    getPointsSound.Play();
+                } else {
+                    //playerLost();
+                    activarProblema();
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.Keypad7) || Input.GetKeyDown(KeyCode.Alpha7)) {
+                if (numeros.Contains(7)) {
+                    Destroy(GameObject.FindGameObjectWithTag("num7"));
+                    numeros.Remove(7);
+                    score += 1;
+                    txt_score.text = score.ToString();
+                    getPointsSound.Play();
+                } else {
+                    //playerLost();
+                    activarProblema();
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.Keypad8) || Input.GetKeyDown(KeyCode.Alpha8)) {
+                if (numeros.Contains(8)) {
+                    Destroy(GameObject.FindGameObjectWithTag("num8"));
+                    numeros.Remove(8);
+                    score += 1;
+                    txt_score.text = score.ToString();
+                    getPointsSound.Play();
+                } else {
+                    //playerLost();
+                    activarProblema();
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.Keypad9) || Input.GetKeyDown(KeyCode.Alpha9)) {
+                if (numeros.Contains(9)) {
+                    Destroy(GameObject.FindGameObjectWithTag("num9"));
+                    numeros.Remove(9);
+                    score += 1;
+                    txt_score.text = score.ToString();
+                    getPointsSound.Play();
+                } else {
+                    //playerLost();
+                    activarProblema();
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return)) {
+                if (playerIsDead == true) {
+                    SceneManager.LoadScene("01");
+                }
+            }
+
+
+            if (Input.GetKeyDown(KeyCode.Escape)) {
+                if (playerIsDead == true) {
+                    SceneManager.LoadScene("00");
+                } else {
+                    if (Time.timeScale == 1) {
+                        NotificationCenter.DefaultCenter().PostNotification(this, "pauseGameplay");
+                    } else {
+                        NotificationCenter.DefaultCenter().PostNotification(this, "resumeGameplay");
+                    }
+
+                }
             }
         }
+
     }
+
+        
 
     private void stopWatch() {
         time += Time.deltaTime;
@@ -266,6 +275,7 @@ public class gameplay : MonoBehaviour {
         lostLiveSound.Play();
         lostLiveCamera.SetActive(true);
         Invoke("disableLostLiveCamera", 0.5f);
+        estaEnElProblema = true;
     }
 
     public void playerLost() {
@@ -314,6 +324,7 @@ public class gameplay : MonoBehaviour {
 
     public void continuarJugando(){
         //Validar primero si la respuesta es correcta
+        estaEnElProblema = false;
         screen_game.SetActive(true);
         screen_problema.SetActive(false);
         NotificationCenter.DefaultCenter().PostNotification(this, "startGenerator");
--- Assets/Scripts/metodos_numericos.cs
+++ Assets/Scripts/metodos_numericos.cs
@@ -1,30 +1,106 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class metodos_numericos : MonoBehaviour{
 
     public TextMeshProUGUI textoCronometro;
     public TextMeshProUGUI textoNombreMetodo;
 
-    public TMP_InputField fieldX1,fieldX2,fieldX3,fieldX4,fieldY1,fieldY2,fieldY3,fieldY4,FieldX;
+    public TMP_InputField FieldX,fieldX1, fieldX2, fieldX3, fieldX4, fieldY1, fieldY2, fieldY3, fieldY4, fieldGx,fieldGxCPU;
 
-    float startingTime = 180f; //420 segundos ~ 7 minutos
+    public GameObject iconGood;
+    public GameObject iconBad;
+
+    public Button btnValidar;
+    public Button btnVerRespuesta;
+    public Button btnRendirse;
+    public Button btnNoAplicaElMetodo;
+
+    float x, x1, x2, x3, x4, y1, y2, y3, y4;
+    float startingTime = 180f;
     float currentTime;
+    float gx;
+
+    bool aplicaMetodo = false;
 
     int opcion;
 
-    void Start(){
-
-    }
-
-
     private void OnEnable() {
 
+        btnNoAplicaElMetodo.onClick.AddListener(delegate {
+
+            btnVerRespuesta.interactable = false;
+            btnRendirse.interactable = false;
+            btnValidar.interactable = false;
+            btnNoAplicaElMetodo.interactable = false;
+
+            if (aplicaMetodo == false) {
+                //Respuesta correcta
+                fieldGx.text = "No aplica el método";
+                iconGood.SetActive(true);
+                Invoke("ganar", 3);
+            } else {
+                //Respuesta incorrecta
+                fieldGx.text = "No aplica el método";
+                iconBad.SetActive(true);
+                Invoke("perder", 3);
+            }
+
+        });
+
+        btnVerRespuesta.onClick.AddListener(delegate {
+            fieldGxCPU.gameObject.SetActive(true);
+            fieldGxCPU.text = gx.ToString("0.0000000");
+            btnVerRespuesta.interactable = false;
+            btnRendirse.interactable = true;
+            btnValidar.interactable = false;
+            btnNoAplicaElMetodo.interactable = false;
+        });
+
+        btnRendirse.onClick.AddListener(delegate {
+            //do something
+        });
+
+        btnValidar.onClick.AddListener(delegate {
+            if (fieldGx.text.Contains(gx.ToString())) {
+                //Respuesta correcta
+                iconGood.SetActive(true);
+                btnVerRespuesta.interactable = false;
+                btnRendirse.interactable = false;
+                btnValidar.interactable = false;
+                btnNoAplicaElMetodo.interactable = false;
+                Invoke("ganar", 3);
+            } else {
+                //Respuesta incorrecta
+                iconBad.SetActive(true);
+                btnVerRespuesta.interactable = false;
+                btnRendirse.interactable = false;
+                btnValidar.interactable = false;
+                btnNoAplicaElMetodo.interactable = false;
+                Invoke("perder", 3);
+            }
+        });
+
+        iconGood.SetActive(false);
+        iconBad.SetActive(false);
+        fieldGxCPU.gameObject.SetActive(false);
+
+        btnVerRespuesta.interactable = true;
+        btnRendirse.interactable = true;
+        btnValidar.interactable = true;
+        btnNoAplicaElMetodo.interactable = true;
+
+        fieldGx.text = "";
+
         currentTime = startingTime;
+
         inicializarDatos();
-        opcion = Random.Range(1, 5); // No incluye el  5, el rango es de 1 a 4
+
+        opcion = UnityEngine.Random.Range(1, 5); // No incluye el  5, el rango es de 1 a 4
 
         switch (opcion) {
             case 1:
@@ -65,56 +141,148 @@ public class metodos_numericos : MonoBehaviour{
 
     private void inicializarDatos() {
 
-        int x = Random.Range(1, 7);
+        //x = UnityEngine.Random.Range(1, 7);
+        x = 3.25f;
 
-        int x1 = Random.Range(1, 7);
-        int x2 = Random.Range(1, 7);
-        int x3 = Random.Range(1, 7);
-        int x4 = Random.Range(1, 7);
+        x1 = UnityEngine.Random.Range(1, 7);
+        x2 = UnityEngine.Random.Range(1, 7);
+        x3 = UnityEngine.Random.Range(1, 7);
+        x4 = UnityEngine.Random.Range(1, 7);
 
-        int y1 = Random.Range(1, 7);
-        int y2 = Random.Range(1, 7);
-        int y3 = Random.Range(1, 7);
-        int y4 = Random.Range(1, 7);
+        y1 = UnityEngine.Random.Range(1, 7);
+        y2 = UnityEngine.Random.Range(1, 7);
+        y3 = UnityEngine.Random.Range(1, 7);
+        y4 = UnityEngine.Random.Range(1, 7);
 
-        FieldX.text = x.ToString();
+        FieldX.text = x.ToString("0.00");
 
-        fieldX1.text = x1.ToString();
-        fieldX2.text = x2.ToString();
-        fieldX3.text = x3.ToString();
-        fieldX4.text = x4.ToString();
+        fieldX1.text = x1.ToString("0.00");
+        fieldX2.text = x2.ToString("0.00");
+        fieldX3.text = x3.ToString("0.00");
+        fieldX4.text = x4.ToString("0.00");
 
-        fieldY1.text = y1.ToString();
-        fieldY2.text = y2.ToString();
-        fieldY3.text = y3.ToString();
-        fieldY4.text = y4.ToString();
+        fieldY1.text = y1.ToString("0.00");
+        fieldY2.text = y2.ToString("0.00");
+        fieldY3.text = y3.ToString("0.00");
+        fieldY4.text = y4.ToString("0.00");
+    }
+
+    float factorial(float n) {
+        int factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+
+    void ganar() {
+        
+        NotificationCenter.DefaultCenter().PostNotification(this, "respuestaCorrecta");  
+    }
+
+    void perder() {
+        NotificationCenter.DefaultCenter().PostNotification(this, "respuestaInorrecta");
     }
 
 
     // =========================== Métodos numericos ========================= //
 
     private void newtonHaciaAdelante() {
+
         textoNombreMetodo.text = "Newton hacia adelante";
-        Debug.Log("Newton hacia adelante");
-        //Aqui empiezan los problemas
-        //NotificationCenter.DefaultCenter().PostNotification(this, "respuestaCorrecta");
-        //NotificationCenter.DefaultCenter().PostNotification(this, "respuestaInorrecta");
+
+        float[] xi = {2.0f, 2.5f, 3.0f,3.5f};
+        float[] yi = {3.03f, 3.48f, 4.08f, 4.87f};
+
+        float h1 = Math.Abs(xi[0] - xi[1]);
+        float h2 = Math.Abs(xi[2] - xi[1]);
+        float h3 = Math.Abs(xi[3] - xi[2]);
+
+        if (h1 == h2 && h1 == h3 && h2 == h3 ) {
+            aplicaMetodo = true;
+        } else {
+            aplicaMetodo = false;
+        }
+
+        float s = (x - xi[0]) / h1;
+        float s0 = 1;
+        float s1 = s;
+        float s2 = (s * (s - 1)) / factorial(2);
+        float s3 = (s * (s - 1) * (s - 2)) / factorial(3);
+        float s4 = (s * (s - 1) * (s - 2) * (s - 3)) / factorial(4);
+
+        //Primera diferencia D'f(xi)
+        float d1_1 = (yi[1] - yi[0]);
+        float d1_2 = (yi[2] - yi[1]);
+        float d1_3 = (yi[3] - yi[2]);
+
+        //Segunda diferencia D''f(xi)
+        float d2_1 = (d1_2 - d1_1);
+        float d2_2 = (d1_3 - d1_2);
+
+        //Tercera diferencia D'''f(xi)
+        float d3_1 = (d2_2 - d2_1);
+
+        gx = (yi[0] * s0) + (d1_1 * s1) + (d2_1 * s2) + (d3_1 * s3);
+
+        Debug.Log("\n\tg(x) = "+gx);
+
     }
 
     private void newtonHaciaAtras() {
+
         textoNombreMetodo.text = "Newton hacia atrás";
-        Debug.Log("Newton hacia atrás");
-       
+
+        float[] xi = { 2.0f, 2.5f, 3.0f, 3.5f };
+        float[] yi = { 3.03f, 3.48f, 4.08f, 4.87f };
+
+        float h1 = Math.Abs(xi[0] - xi[1]);
+        float h2 = Math.Abs(xi[2] - xi[1]);
+        float h3 = Math.Abs(xi[3] - xi[2]);
+
+        if (h1 == h2 && h1 == h3 && h2 == h3) {
+            aplicaMetodo = true;
+        } else {
+            aplicaMetodo = false;
+        }
+
+        float s = (x - xi[3]) / h1;
+        float s0 = 1;
+        float s1 = s;
+        float s2 = (s * (s + 1)) / factorial(2);
+        float s3 = (s * (s + 1) * (s + 2)) / factorial(3);
+        float s4 = (s * (s + 1) * (s + 2) * (s + 3)) / factorial(4);
+
+        //Primera diferencia D'f(xi)
+        float d1_1 = (yi[1] - yi[0]);
+        float d1_2 = (yi[2] - yi[1]);
+        float d1_3 = (yi[3] - yi[2]);
+
+        //Segunda diferencia D''f(xi)
+        float d2_1 = (d1_2 - d1_1);
+        float d2_2 = (d1_3 - d1_2);
+
+        //Tercera diferencia D'''f(xi)
+        float d3_1 = (d2_2 - d2_1);
+
+        gx = yi[3] * s0 + (d1_3 * s1) + (d2_2 * s2) + (d3_1 * s3);
+
+        Debug.Log("\n\tg(x) = " + gx);
+
     }
 
     private void newtonDiferenciasDivididas() {
         textoNombreMetodo.text = "Newton diferencias divididas";
-        Debug.Log("Newton diferencias divididas");
+
+        aplicaMetodo = true;
+        
     }
 
     private void laGrange() {
         textoNombreMetodo.text = "LaGrange";
-        Debug.Log("LaGrange");
+
+        aplicaMetodo = true;
+
     }
 
 }
